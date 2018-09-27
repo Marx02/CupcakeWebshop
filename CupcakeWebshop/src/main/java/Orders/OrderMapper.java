17b94e7807dd78f91398c3d07f88f567783e8cc7
Marx02 <casper_marx@msn.com>
2018-09-27 10:36:03 +0200

    NAME ON PAGE <33333 ordermapperchange

--- CupcakeWebshop/src/main/java/Orders/OrderMapper.java
+++ CupcakeWebshop/src/main/java/Orders/OrderMapper.java
@@ -17,30 +17,6 @@ import java.sql.Statement;
  */
 public class OrderMapper {
 
-    public User getUserInfo(String uName, String password) {
-        User aUser = null;
-        try {
-            Connection c = new DBConnector().getConnection();
-            Statement st = c.createStatement();
-            String query
-                    = "SELECT pw FROM users WHERE username = '" + uName + "';";
-            ResultSet res = st.executeQuery(query);
-            while (res.next()) {
-                String pass = res.getString("pw");
-                if (pass.equals(password)) {
-                    aUser = new User("Works", 100);
-                } else {
-                    aUser = new User("Wrong pass", 0);
-                }
-            }
-        } catch (Exception ex) {
-            aUser = new User("Error", 0);
-            return aUser;
-
-        }
-        return aUser;
-    }
-
     public Order getOrderByID(int orderID, int userID, int price, int qty, String pname, String username, String customername) {
         Order oID = null;
         try {
@@ -49,7 +25,7 @@ public class OrderMapper {
             String query
                     = "SELECT * "
                     + "FROM orders "
-                    + "WHERE orderID = '"+ oID +"';" ;
+                    + "WHERE orderID = '" + oID + "';";
             ResultSet res = st.executeQuery(query);
             userID = res.getInt(orderID);
             price = res.getInt("price");
@@ -57,11 +33,8 @@ public class OrderMapper {
         } catch (Exception ex) {
             return null;
         }
-            
-        
-        
+
         return oID;
     }
-    
-    
+
 }
