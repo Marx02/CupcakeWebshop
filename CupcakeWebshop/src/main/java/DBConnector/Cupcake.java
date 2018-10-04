5129a5faf2ff08fdf12b182cc28ef96fcadaf2aa
Edunno <tamericandream@hotmail.com>
2018-10-04 13:05:28 +0200

    And now for Javadoc

--- CupcakeWebshop/src/main/java/DBConnector/Cupcake.java
+++ CupcakeWebshop/src/main/java/DBConnector/Cupcake.java
@@ -14,16 +14,35 @@ public class Cupcake {
     public CupcakeTopping topping;
     public CupcakeBottom bottom;
 
+    /**
+     *Takes a <code>CupcakeTopping</code> and a <code>CupcakeBottom</code>
+     * and combine them into a object.
+     * 
+     * @param topping
+     * @param bottom
+     */
     public Cupcake(CupcakeTopping topping, CupcakeBottom bottom) {
         this.topping = topping;
         this.bottom = bottom;
 
     }
 
+    /**
+     *Generates the name of the cupcake, combined from top and
+     * bottom of the requested cupcake.
+     * 
+     * @return String Name
+     */
     public String getName() {
         return bottom.getName() + " " + topping.getName();
     }
 
+    /**
+     * Generates the total price of the cupcake, combined from
+     * top and bottom of the requested cupcake.
+     *
+     * @return int price of the cupcake
+     */
     public int getPrice() {
         return bottom.getPrice() + topping.getPrice();
     }
