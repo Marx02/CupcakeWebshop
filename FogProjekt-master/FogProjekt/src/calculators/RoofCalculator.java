ddb1579b39b2a32438575f3a856f0546d1823eca
Marx02 <casper_marx@msn.com>
2018-11-13 09:08:32 +0100

    Added RoofCalculator

---
+++ FogProjekt-master/FogProjekt/src/calculators/RoofCalculator.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fogstarter;
+
+/**
+ *
+ * @author caspe
+ */
+public class RoofCalculator {
+    
+    public int RoofCalc (double a, double b){
+        int rc = (int) (a * b);
+        return rc;
+    }
+    
+}
